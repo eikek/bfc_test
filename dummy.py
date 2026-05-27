759fe5383796f4e313157e91076292193afa8069
Eike <eikek@posteo.de>
2026-05-27 15:51:40 +0200

    make script use arguments

--- dummy.py
+++ dummy.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 
 def dummy_count(max: int) -> None:
@@ -6,6 +7,16 @@ def dummy_count(max: int) -> None:
         time.sleep(1)
 
 
+def main():
+    parser = argparse.ArgumentParser(description="Testing")
+    parser.add_argument("-n", "--count", type=int, default=10)
+    parser.add_argument("-x", "--exit", type=int, default=0)
+
+    args = parser.parse_args()
+
+    dummy_count(args.count)
+    if args.exit > 0:
+        raise SystemExit(args.exit)
+
 if __name__ == "__main__":
-    dummy_count(10)
-    print("Done.")
+    main()
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell-tools.url = "github:eikek/devshell-tools";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    devshell-tools,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      devshellPkgs = with pkgs; [
+        python3
+      ];
+    in {
+      formatter = pkgs.alejandra;
+      devShells = {
+        default = pkgs.mkShellNoCC {
+          buildInputs = devshellPkgs;
+        };
+      };
+    });
+}
